8f715924504c2714dffeec5e7f37fdc370e25352
przemelek <przemelek@gmail.com>
2020-04-14 00:35:35 +0200

    Added test version of my toy app for checklists, for now have only 2 baked in lists, in Polish ;-)

---
+++ checklist/precache-manifest.bf178e2b8a86f73b7b1a988c9b812cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfd7ca238d72bf9df41c92b2dbfd2b8",
+    "url": "/checklist/index.html"
+  },
+  {
+    "revision": "30d55724fdacf9235e9b",
+    "url": "/checklist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8dd372747a7cfd1b0e43",
+    "url": "/checklist/static/js/2.91057342.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/checklist/static/js/2.91057342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d55724fdacf9235e9b",
+    "url": "/checklist/static/js/main.faa9f3c8.chunk.js"
+  },
+  {
+    "revision": "9f7b6fa169828f9503b4",
+    "url": "/checklist/static/js/runtime-main.360f646c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/checklist/static/media/logo.5d5d9eef.svg"
+  }
+]);
